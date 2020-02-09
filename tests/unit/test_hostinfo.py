b077798f88851ae4660bf7983f4121981e7f95f8
mariojmdavid <mariojmdavid@gmail.com>
2020-02-09 13:58:06 +0000

    wip hostinfo

--- tests/unit/test_hostinfo.py
+++ tests/unit/test_hostinfo.py
@@ -4,10 +4,28 @@
 """
 udocker unit tests: HostInfo
 """
+import pwd
+import sys
 import pytest
+
+try:
+    import udocker
+except ImportError:
+    sys.path.append('.')
+    sys.path.append('../')
+    import udocker
+
 from udocker.helper.hostinfo import HostInfo
 
-def test_username():
+##@pytest.mark.parametrize
+def test_username(monkeypatch):
     """Test001 HostInfo.username"""
+    def mock_pw_name():
+        return 'someuser'
+
+    monkeypatch.setattr(pwd.getpwuid, 'pw_name', mock_pw_name)
+    uname = mock_pw_name()
     hinfo = HostInfo()
-    assert hinfo.username() == 'david'
+    assert hinfo.username() == 'someuser'
+    # with pytest.raises(KeyError):
+    #     hinfo.username()
