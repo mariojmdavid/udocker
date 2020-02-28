f8d968ad487f054c3a1de8db109befe69c4edf19
mariojmdavid <mariojmdavid@gmail.com>
2020-02-28 15:59:08 +0000

    test_oci passing

--- tests/unit/test_oci.py
+++ tests/unit/test_oci.py
@@ -69,8 +69,24 @@ class OciLocalFileAPITestCase(TestCase):
         status = OciLocalFileAPI(self.local)._get_from_manifest(struct, imgtag)
         self.assertEqual(status, (conf_out, lay_out))
 
-    # def test_04__load_manifest(self):
+    # @patch('udocker.oci.Unique.imagename')
+    # @patch('udocker.oci.Unique.imagetag')
+    # @patch('udocker.container.localrepo.LocalRepository.load_json', autospec=True)
+    # def test_04__load_manifest(self, mock_ljson, mock_uniqtag, mock_uniqname):
     #     """Test04 OciLocalFileAPI()._load_manifest."""
+    #     manifest = {'annotations': {'org.opencontainers.image.ref.name': '123'}}
+    #     mock_uniqtag.return_value = '123'
+    #     mock_uniqname.return_value = 'imgname'
+    #     mock_ljson.return_value = {'layers': [{'digest': 'd1'},
+    #                                           {'digest': 'd2'}],
+    #                                'config': {'digest': 'dgt'}}
+    #     struct = {'manifest': {'123': {'json': mock_ljson}},
+    #               'repolayers': {'digest': {'layer_a': 'f1',
+    #                                         'layer_f': 'tmpimg/blobs/f1/f2',
+    #                                         'layer_h': 'f2'}}
+    #              }
+    #     status = OciLocalFileAPI(self.local)._load_manifest(struct, manifest)
+    #     self.assertEqual(status, ['123'])
 
     # def test_05__load_repositories(self):
     #     """Test05 OciLocalFileAPI()._load_repositories."""
@@ -78,5 +94,30 @@ class OciLocalFileAPITestCase(TestCase):
     # def test_06__load_image_step2(self):
     #     """Test07 OciLocalFileAPI()._load_image_step2."""
 
-    # def test_07_load(self):
-    #     """Test07 OciLocalFileAPI().load."""
+    @patch('udocker.oci.Msg.err')
+    @patch.object(OciLocalFileAPI, '_load_repositories')
+    @patch.object(OciLocalFileAPI, '_load_structure')
+    def test_07_load(self, mock_loadstruct, mock_loadrepo, mock_msg):
+        """Test07 OciLocalFileAPI().load."""
+        tmpdir = '/ROOT'
+        imgrepo = 'somerepo'
+        mock_loadstruct.return_value = {}
+        status = OciLocalFileAPI(self.local).load(tmpdir, imgrepo)
+        self.assertTrue(mock_msg.called)
+        self.assertEqual(status, [])
+
+        tmpdir = '/ROOT'
+        imgrepo = 'somerepo'
+        mock_loadstruct.return_value = {'repolayers': {'f1:f2': {'layer_a': 'f1',
+                                                                 'layer_f': 'tmpimg/blobs/f1/f2',
+                                                                 'layer_h': 'f2'}},
+                                        'manifest': {},
+                                        'oci-layout': 'oci_lay1',
+                                        'index': 'idx1'}
+        mock_loadrepo.return_value = ['r1', 'r2']
+        status = OciLocalFileAPI(self.local).load(tmpdir, imgrepo)
+        self.assertEqual(status, ['r1', 'r2'])
+
+
+if __name__ == '__main__':
+    main()
