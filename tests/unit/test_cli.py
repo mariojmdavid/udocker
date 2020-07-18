27e64c61bdb05819c42f3cb29004f157773357ac
mariojmdavid <mariojmdavid@gmail.com>
2020-07-18 19:05:09 +0100

    wipp tests cli

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -6,6 +6,9 @@ udocker unit tests: UdockerCLI
 """
 
 import sys
+sys.path.append('.')
+sys.path.append('../../')
+
 from udocker.config import Config
 from udocker.cmdparser import CmdParser
 from udocker.cli import UdockerCLI
@@ -38,7 +41,7 @@ class UdockerCLITestCase(TestCase):
         Config().conf['oskernel'] = "4.8.13"
         Config().conf['location'] = ""
         Config().conf['keystore'] = "KEYSTORE"
-        self.cmdp = CmdParser()
+
         str_local = 'udocker.container.localrepo.LocalRepository'
         self.lrepo = patch(str_local)
         self.local = self.lrepo.start()
@@ -53,78 +56,143 @@ class UdockerCLITestCase(TestCase):
     @patch('udocker.cli.DockerIoAPI')
     def test_01_init(self, mock_dioapi, mock_ks, mock_lfapi):
         """Test01 UdockerCLI() constructor."""
-
         # Test Config().conf['keystore'] starts with /
         Config().conf['keystore'] = "/xxx"
         udoc = UdockerCLI(self.local)
         self.assertTrue(mock_dioapi.called)
         self.assertTrue(mock_lfapi.called)
         self.assertTrue(mock_ks.called_with(Config().conf['keystore']))
-        mock_ks.reset_mock()
 
         # Test Config().conf['keystore'] does not starts with /
         Config().conf['keystore'] = "xx"
         udoc = UdockerCLI(self.local)
         self.assertTrue(mock_ks.called_with(Config().conf['keystore']))
 
-    def test_02__cdrepo(self):
+    @patch('udocker.cli.FileUtil.isdir')
+    def test_02__cdrepo(self, mock_isdir):
         """Test02 UdockerCLI()._cdrepo()."""
+        argv = ["udocker", "-h"]
+        cmdp = CmdParser()
+        cmdp.parse(argv)
+        udoc = UdockerCLI(self.local)
+        status = udoc._cdrepo(cmdp)
+        self.assertFalse(status)
+        self.assertFalse(mock_isdir.called)
 
-    # @patch('udocker.cli.KeyStore')
-    # @patch('udocker.cli.DockerIoAPI')
-    # @patch('udocker.cli.Msg')
-    # def test_03__check_imagespec(self, mock_msg, mock_dioapi, mock_ks):
-    #     """Test03 UdockerCLI()._check_imagespec()."""
+        argv = ["udocker"]
+        cmdp = CmdParser()
+        cmdp.parse(argv)
+        mock_isdir.return_value = False
+        udoc = UdockerCLI(self.local)
+        status = udoc._cdrepo(cmdp)
+        self.assertFalse(status)
+        self.assertTrue(mock_isdir.called)
 
-    #     mock_msg.level = 0
-    #     mock_dioapi.is_repo_name = False
-    #     udoc = UdockerCLI(self.local)
-    #     status = udoc._check_imagespec("")
-    #     self.assertEqual(status, (None, None))
+        argv = ["udocker"]
+        cmdp = CmdParser()
+        cmdp.parse(argv)
+        mock_isdir.return_value = True
+        self.local.setup.return_value = None
+        udoc = UdockerCLI(self.local)
+        status = udoc._cdrepo(cmdp)
+        self.assertTrue(status)
+        self.assertTrue(self.local.setup.called)
 
-    #     mock_dioapi.is_repo_name = False
-    #     udoc = UdockerCLI(self.local)
-    #     status = udoc._check_imagespec("AAA")
-    #     self.assertEqual(status, ("AAA", "latest"))
+    @patch('udocker.cli.DockerIoAPI.is_repo_name')
+    @patch('udocker.cli.Msg')
+    def test_03__check_imagespec(self, mock_msg, mock_reponame):
+        """Test03 UdockerCLI()._check_imagespec()."""
+        mock_msg.level = 0
+        mock_reponame.return_value = False
+        udoc = UdockerCLI(self.local)
+        status = udoc._check_imagespec("")
+        self.assertEqual(status, (None, None))
 
-    #     mock_dioapi.is_repo_name = False
-    #     udoc = UdockerCLI(self.local)
-    #     status = udoc._check_imagespec("AAA:45")
-    #     self.assertEqual(status, ("AAA", "45"))
+        mock_reponame.return_value = True
+        udoc = UdockerCLI(self.local)
+        status = udoc._check_imagespec("AAA")
+        self.assertEqual(status, ("AAA", "latest"))
 
-    # def test_04__check_imagerepo(self):
-    #     """Test04 UdockerCLI()._check_imagerepo()."""
+        mock_reponame.return_value = True
+        udoc = UdockerCLI(self.local)
+        status = udoc._check_imagespec("AAA:45")
+        self.assertEqual(status, ("AAA", "45"))
+
+    @patch('udocker.cli.DockerIoAPI.is_repo_name')
+    @patch('udocker.cli.Msg')
+    def test_04__check_imagerepo(self, mock_msg, mock_reponame):
+        """Test04 UdockerCLI()._check_imagerepo()."""
+        mock_msg.level = 0
+        mock_reponame.return_value = False
+        udoc = UdockerCLI(self.local)
+        status = udoc._check_imagerepo("")
+        self.assertEqual(status, None)
+
+        mock_reponame.return_value = True
+        udoc = UdockerCLI(self.local)
+        status = udoc._check_imagerepo("AAA")
+        self.assertEqual(status, "AAA")
 
     # def test_05__set_repository(self):
     #     """Test05 UdockerCLI()._set_repository()."""
 
-    # def test_06__split_imagespec(self):
-    #     """Test06 UdockerCLI()._split_imagespec()."""
+    def test_06__split_imagespec(self):
+        """Test06 UdockerCLI()._split_imagespec()."""
+        imgrepo = ""
+        res = ("", "", "", "")
+        udoc = UdockerCLI(self.local)
+        status = udoc._split_imagespec(imgrepo)
+        self.assertEqual(status, res)
 
-    # @patch('udocker.cmdparser.CmdParser.get', autospec=True)
-    # @patch('udocker.cli.os.path.exists')
-    # @patch('udocker.cli.Msg')
-    # def test_07_do_mkrepo(self, mock_msg, mock_exists, mock_cmdget):
-    #     """Test07 UdockerCLI().do_mkrepo()."""
+        imgrepo = "dockerhub.io/myimg:1.2"
+        res = ("", "dockerhub.io", "myimg", "1.2")
+        udoc = UdockerCLI(self.local)
+        status = udoc._split_imagespec(imgrepo)
+        self.assertEqual(status, res)
 
-    #     mock_msg.level = 0
-    #     mock_cmdget.return_value = "/"
-    #     mock_exists.return_value = True
-    #     udoc = UdockerCLI(self.local)
-    #     status = udoc.do_mkrepo(self.cmdp)
-    #     self.assertEqual(status, 1)
+        imgrepo = "https://dockerhub.io/myimg:1.2"
+        res = ("https:", "dockerhub.io", "myimg", "1.2")
+        udoc = UdockerCLI(self.local)
+        status = udoc._split_imagespec(imgrepo)
+        self.assertEqual(status, res)
 
-    #     mock_cmdget.return_value = ""
-    #     mock_exists.return_value = True
-    #     udoc = UdockerCLI(self.local)
-    #     status = udoc.do_mkrepo(self.cmdp)
-    #     # self.assertEqual(status, 0)
+    @patch('udocker.cli.os.path.exists')
+    @patch('udocker.cli.Msg')
+    def test_07_do_mkrepo(self, mock_msg, mock_exists):
+        """Test07 UdockerCLI().do_mkrepo()."""
+        mock_msg.level = 0
 
-    #     mock_cmdget.return_value = "/"
-    #     mock_exists.return_value = True
-    #     udoc = UdockerCLI(self.local)
-    #     status = udoc.do_mkrepo(self.cmdp)
-    #     self.assertEqual(status, 1)
+        argv = ["udocker", "-h"]
+        cmdp = CmdParser()
+        cmdp.parse(argv)
+        mock_exists.return_value = True
+        udoc = UdockerCLI(self.local)
+        status = udoc.do_mkrepo(cmdp)
+        self.assertEqual(status, 1)
+        self.assertFalse(mock_exists.called)
+
+        argv = ["udocker", "mkrepo"]
+        cmdp = CmdParser()
+        cmdp.parse(argv)
+        mock_exists.return_value = False
+        self.local.setup.return_value = None
+        self.local.create_repo.return_value = False
+        udoc = UdockerCLI(self.local)
+        status = udoc.do_mkrepo(cmdp)
+        self.assertEqual(status, 1)
+        self.assertTrue(mock_exists.called)
+        self.assertTrue(self.local.setup.called)
+        self.assertTrue(self.local.create_repo.called)
+
+        argv = ["udocker", "mkrepo"]
+        cmdp = CmdParser()
+        cmdp.parse(argv)
+        mock_exists.return_value = False
+        self.local.setup.return_value = None
+        self.local.create_repo.return_value = True
+        udoc = UdockerCLI(self.local)
+        status = udoc.do_mkrepo(cmdp)
+        self.assertEqual(status, 0)
 
     # def test_08__search_print_lines(self):
     #     """Test08 UdockerCLI()._search_print_lines()."""
@@ -132,8 +200,13 @@ class UdockerCLITestCase(TestCase):
     # def test_09__search_repositories(self):
     #     """Test09 UdockerCLI()._search_repositories()."""
 
-    # def test_10__list_tags(self):
-    #     """Test10 UdockerCLI()._list_tags()."""
+    @patch('udocker.cli.DockerIoAPI.get_tags')
+    def test_10__list_tags(self, mock_gettags):
+        """Test10 UdockerCLI()._list_tags()."""
+        mock_gettags.return_value = ["t1"]
+        udoc = UdockerCLI(self.local)
+        status = udoc._list_tags("t1")
+        self.assertEqual(status, 0)
 
     # def test_11_do_search(self):
     #     """Test11 UdockerCLI().do_search()."""
